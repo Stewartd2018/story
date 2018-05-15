96525a252e4a46884f1407c776ade26b29367c15
Stewartd2018 <dstewart@sjnma.org>
2018-05-15 09:11:16 -0500

    my story

---
+++ hello/main.js
@@ -0,0 +1,17 @@
+alert("hello");
+alert("oranges");
+console.log("bananas");
+
+var noun1 = prompt("Please enter a noun", "");
+var noun2 = prompt("Please enter a noun", "");
+var noun3 = prompt("Please enter a noun", "");
+var noun4 = prompt("Please enter a noun", "");
+var verb1 = prompt("Please enter a past tense verb", "");
+var verb2 = prompt("Please enter a past tense verb", "");
+var verb3 = prompt("Please enter a past tense verb", "");
+var verb4 = prompt("Please enter a past tense verb", "");
+var verb5 = prompt("Please enter a past tense verb", "");
+var adjective1 = prompt("Please enter an adjective", "");
+var adjective2 = prompt("Please enter an adjective", "");
+
+document.getElementById("story").innerHTML="There once was a " + noun1 + " who " + verb1 + " in New York. One day a " + noun2 + " saw " + noun1 + " and said 'YOU OWE ME MONEY', so he " + verb2 + " him in the face. After that happened he " + verb3 + " to the nearest " + adjective2 + " coffee shop, where " + noun2 + " met him and gave him a " + noun3 + " and said he was sorry. So they " + verb4 + " off the top of the coffee shop and flew away to " + verb5 + " on a giant " + adjective1 + " " +  noun4 + "."; 
